e29038b05877f19ede13cd7428eb1d042f2f7b53
nyzd <abedi.mahdi.dev@gmail.com>
2025-10-26 18:13:04 +0330

    Removed old code

--- src/app/discover/search/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { getSurahs } from "@/actions/getSurahs";
-import { SearchPageClient } from "./SearchPageClient";
-
-export default async function Page() {
-    const surahList = await getSurahs("hafs");
-
-    return <SearchPageClient initialSurahList={surahList} />;
-}
