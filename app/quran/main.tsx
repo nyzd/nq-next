252e8b18a30cd57409fc246f747c4ff1aaf2ab2e
nyzd <abedi.mahdi.dev@gmail.com>
2025-11-05 18:17:04 +0330

    FIX

--- app/quran/main.tsx
+++ app/quran/main.tsx
@@ -1,12 +1,9 @@
 import Mushaf from "@/components/quran/Mushaf";
-import { Suspense } from "react";
 
 export default function Main() {
     return (
         <main className="p-10 flex flex-col items-center justify-center gap-2">
-            <Suspense fallback={<h1>Loading Mushaf</h1>}>
-                <Mushaf name="hafs" />
-            </Suspense>
+            <Mushaf name="hafs" />
         </main>
     );
 }
