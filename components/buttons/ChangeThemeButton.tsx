5117f0600b70c3de024bba9846e86a46f9c7db55
nyzd <azerothadev@gmail.com>
2025-12-31 19:17:50 +0330

    acordion added to the mushaf settings

--- components/buttons/ChangeThemeButton.tsx
+++ components/buttons/ChangeThemeButton.tsx
@@ -4,41 +4,26 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 import { Symbol } from "@yakad/symbols";
 
 export function ChangeThemeButton() {
     const { theme, setTheme } = useTheme();
 
+    const changeThemeOnclick = () => {
+        if (theme === "light") setTheme("dark");
+        if (theme === "dark") setTheme("system");
+        if (theme === "system") setTheme("light");
+    }
+
+
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon-lg" className="w-full">
-                    <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-                    <span className="sr-only">Toggle theme</span>
-                    {theme}
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    <Symbol icon="light_mode" />
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    <Symbol icon="mode_night" />
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    <Symbol icon="computer" />
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Button variant="outline" size="icon-lg" className="w-full" onClick={changeThemeOnclick}>
+            {theme === "light" ?
+                <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+                : theme === "dark" ?
+                    <Moon className="h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" /> : <Symbol icon="computer" />}
+            <span className="sr-only">Toggle theme</span>
+            {theme}
+        </Button>
     );
 }
--- components/buttons/MushafSettingsButton.tsx
+++ components/buttons/MushafSettingsButton.tsx
@@ -23,6 +23,12 @@ import { Symbol } from "@yakad/symbols";
 import { ChangeThemeButton } from "./ChangeThemeButton";
 import { useWindowSize } from "../useWindowSize";
 import { Separator } from "../ui/separator";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "../ui/accordion";
 
 export function MushafSettingsButton({
     translations,
@@ -52,151 +58,180 @@ export function MushafSettingsButton({
                 </SheetHeader>
 
                 <div className="space-y-6 p-7">
-                    <div className="space-y-4">
-                        <Label className="text-base font-semibold">
-                            <Symbol icon="match_case" />
-                            Arabic Text
-                        </Label>
-
-                        <div className="flex gap-2 justify-around">
-                            <div className="space-y-2">
-                                <Label htmlFor="arabicFont" className="text-sm">
-                                    <Symbol icon="brand_family" />
-                                    Font
+                    <Accordion
+                        type="single"
+                        collapsible
+                        defaultValue="item-1"
+                    >
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger className="hover:no-underline cursor-pointer hover:bg-accent/80 p-3">
+                                <Label className="text-base font-semibold">
+                                    <Symbol icon="match_case" />
+                                    Arabic Text
                                 </Label>
-                                <Select
-                                    value={"tahoma"}
-                                    onValueChange={(value) =>
-                                        handleSelectChange("arabicFont", value)
-                                    }
-                                >
-                                    <SelectTrigger id="arabicFont">
-                                        <SelectValue placeholder="Font" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="tahoma">
-                                            Tahoma
-                                        </SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
+                            </AccordionTrigger>
+                            <div className="flex gap-2 justify-around">
+                                <div className="space-y-2">
+                                    <Label htmlFor="arabicFont" className="text-sm">
+                                        <Symbol icon="brand_family" />
+                                        Font
+                                    </Label>
+                                    <Select
+                                        value={"tahoma"}
+                                        onValueChange={(value) =>
+                                            handleSelectChange("arabicFont", value)
+                                        }
+                                    >
+                                        <SelectTrigger id="arabicFont">
+                                            <SelectValue placeholder="Font" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="tahoma">
+                                                Tahoma
+                                            </SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
 
-                            <div className="space-y-2">
-                                <Label
-                                    htmlFor="arabicFontSize"
-                                    className="text-sm"
-                                >
-                                    <Symbol icon="format_size" />
-                                    Font size
-                                </Label>
-                                <Select
-                                    value={mushafOptions.arabicFontSize}
-                                    onValueChange={(value) =>
-                                        handleSelectChange(
-                                            "arabicFontSize",
-                                            value
-                                        )
-                                    }
-                                >
-                                    <SelectTrigger id="arabicFontSize">
-                                        <SelectValue placeholder="Font size" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="small">
-                                            Small
-                                        </SelectItem>
-                                        <SelectItem value="medium">
-                                            Medium
-                                        </SelectItem>
-                                        <SelectItem value="large">
-                                            Large
-                                        </SelectItem>
-                                    </SelectContent>
-                                </Select>
-                            </div>
+                                <div className="space-y-2">
+                                    <Label
+                                        htmlFor="arabicFontSize"
+                                        className="text-sm"
+                                    >
+                                        <Symbol icon="format_size" />
+                                        Font size
+                                    </Label>
+                                    <Select
+                                        value={mushafOptions.arabicFontSize}
+                                        onValueChange={(value) =>
+                                            handleSelectChange(
+                                                "arabicFontSize",
+                                                value
+                                            )
+                                        }
+                                    >
+                                        <SelectTrigger id="arabicFontSize">
+                                            <SelectValue placeholder="Font size" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="small">
+                                                Small
+                                            </SelectItem>
+                                            <SelectItem value="medium">
+                                                Medium
+                                            </SelectItem>
+                                            <SelectItem value="large">
+                                                Large
+                                            </SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
 
-                            <div className="space-y-2">
-                                <Label htmlFor="textAlign" className="text-sm">
-                                    <Symbol icon="article" />
-                                    Text align
-                                </Label>
-                                <Select
-                                    value={mushafOptions.textAlign}
-                                    onValueChange={(value) =>
-                                        handleSelectChange("textAlign", value)
-                                    }
-                                >
-                                    <SelectTrigger id="textAlign">
-                                        <SelectValue placeholder="Text align" />
-                                    </SelectTrigger>
-                                    <SelectContent>
-                                        <SelectItem value="normal">
-                                            <Symbol icon="format_align_right" />
-                                            Normal
-                                        </SelectItem>
-                                        <SelectItem value="center">
-                                            <Symbol icon="format_align_center" />
-                                            Center
-                                        </SelectItem>
-                                    </SelectContent>
-                                </Select>
+                                <div className="space-y-2">
+                                    <Label htmlFor="textAlign" className="text-sm">
+                                        <Symbol icon="article" />
+                                        Text align
+                                    </Label>
+                                    <Select
+                                        value={mushafOptions.textAlign}
+                                        onValueChange={(value) =>
+                                            handleSelectChange("textAlign", value)
+                                        }
+                                    >
+                                        <SelectTrigger id="textAlign">
+                                            <SelectValue placeholder="Text align" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="normal">
+                                                <Symbol icon="format_align_right" />
+                                                Normal
+                                            </SelectItem>
+                                            <SelectItem value="center">
+                                                <Symbol icon="format_align_center" />
+                                                Center
+                                            </SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <Separator />
+                    <Accordion
+                        type="single"
+                        collapsible
+                        defaultValue="item-1"
+                    >
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger className="hover:no-underline cursor-pointer hover:bg-accent/80 p-3">
+                                <Label className="text-base font-semibold">
+                                    <Symbol icon="translate" />
+                                    Translate
+                                </Label>
+                            </AccordionTrigger>
+
+                            <AccordionContent className="space-y-8 mt-5">
+                                <SelectTranslation translations={translations} />
+
+                                <div className="space-y-2">
+                                    <Label
+                                        htmlFor="translationFontSize"
+                                        className="text-sm"
+                                    >
+                                        Translation Font size
+                                    </Label>
+                                    <Select
+                                        value={mushafOptions.translationFontSize}
+                                        onValueChange={(value) =>
+                                            handleSelectChange(
+                                                "translationFontSize",
+                                                value
+                                            )
+                                        }
+                                    >
+                                        <SelectTrigger
+                                            id="translationFontSize"
+                                            className="w-full"
+                                        >
+                                            <SelectValue placeholder="Translation Font size" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            <SelectItem value="small">Small</SelectItem>
+                                            <SelectItem value="medium">
+                                                Medium
+                                            </SelectItem>
+                                            <SelectItem value="large">Large</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                </div>
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
 
                     <Separator />
 
-                    <div className="space-y-4">
-                        <Label className="text-base font-semibold">
-                            <Symbol icon="translate" />
-                            Translate
-                        </Label>
-
-                        <SelectTranslation translations={translations} />
-
-                        <div className="space-y-2">
-                            <Label
-                                htmlFor="translationFontSize"
-                                className="text-sm"
-                            >
-                                Translation Font size
-                            </Label>
-                            <Select
-                                value={mushafOptions.translationFontSize}
-                                onValueChange={(value) =>
-                                    handleSelectChange(
-                                        "translationFontSize",
-                                        value
-                                    )
-                                }
-                            >
-                                <SelectTrigger
-                                    id="translationFontSize"
-                                    className="w-full"
-                                >
-                                    <SelectValue placeholder="Translation Font size" />
-                                </SelectTrigger>
-                                <SelectContent>
-                                    <SelectItem value="small">Small</SelectItem>
-                                    <SelectItem value="medium">
-                                        Medium
-                                    </SelectItem>
-                                    <SelectItem value="large">Large</SelectItem>
-                                </SelectContent>
-                            </Select>
-                        </div>
-                    </div>
-
-                    <Separator />
-                    <div className="space-y-4">
-                        <Label className="text-base font-semibold">
-                            <Symbol icon="colors" />
-                            Theme
-                        </Label>
-                        <ChangeThemeButton />
-                    </div>
+                    <Accordion
+                        type="single"
+                        collapsible
+                        defaultValue="item-1"
+                    >
+                        <AccordionItem value="item-1">
+                            <AccordionTrigger className="hover:no-underline cursor-pointer hover:bg-accent/80 p-3">
+                                <div className="space-y-4">
+                                    <Label className="text-base font-semibold">
+                                        <Symbol icon="colors" />
+                                        Theme
+                                    </Label>
+                                </div>
+                            </AccordionTrigger>
+                            <AccordionContent className="p-4">
+                                <ChangeThemeButton />
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
                 </div>
             </SheetContent>
-        </Sheet>
+        </Sheet >
     );
 }
